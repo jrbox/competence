8b8bd35b4e6de68687e6cfc449e0d6be9253fa96
jrbox <junior.maillet@gmail.com>
2022-11-25 16:59:03 +0100

    microservice competence

--- competences/src/main/java/fr/mycomp/competences/EquipeRepository.java
+++ competences/src/main/java/fr/mycomp/competences/EquipeRepository.java
@@ -1,15 +1,19 @@
 package fr.mycomp.competences;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
 @Repository
 public class EquipeRepository {
 
+    @Value("${EQUIPES_URL:http://equipes:3000/api/equipes}")
+    private String equipeUrl;
+
     private RestTemplate restTemplate = new RestTemplate();
     public void addToEquipe(Competence competence) {
         if(competence != null && competence.getEquipeId() != null) {
-            String url = "http://localhost:3000/api/equipes/"+competence.getEquipeId()+"/competences";
+            String url = equipeUrl+"/"+competence.getEquipeId()+"/competences";
             restTemplate.put(
                     url
                     , competence, Void.class);
@@ -18,7 +22,7 @@ public class EquipeRepository {
     
     public void removeToEquipe(Competence competence) {
         if(competence != null && competence.getEquipeId() != null) {
-            String url = "http://localhost:3000/api/equipes/"+competence.getEquipeId()+"/competences/"+competence.getId();
+            String url = equipeUrl+"/"+competence.getEquipeId()+"/competences/"+competence.getId();
             restTemplate.delete(
                     url
                     , competence, Void.class);
